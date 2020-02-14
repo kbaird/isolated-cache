d81ab039b7f3f01610ae275bcc258357723a483c
Kevin Baird <kevincbaird@gmail.com>
2020-02-14 11:47:40 -0800

    Break up tests

--- test/cache_test.exs
+++ test/cache_test.exs
@@ -8,14 +8,13 @@ defmodule CacheTest do
     {:ok, cache: cache}
   end
 
-  ### TODO: break these up
-  test "it stores distinct sets under distinct keys", %{cache: cache} do
+  test "it returns nil for novel keys", %{cache: cache} do
     assert Cache.get(cache, :key) == nil
     assert Cache.get(cache, :other_key) == nil
+  end
 
-    :ok = Cache.put(cache, :key, "value1")
-    :ok = Cache.put(cache, :key, "value2")
-    :ok = Cache.put(cache, :other_key, "other value")
+  test "it stores distinct sets under distinct keys", %{cache: cache} do
+    cache = initial_writes(cache)
     values_under_key = Cache.get(cache, :key)
     values_under_other_key = Cache.get(cache, :other_key)
 
@@ -26,13 +25,26 @@ defmodule CacheTest do
     assert "other value" in values_under_other_key
     refute "value1" in values_under_other_key
     refute "value2" in values_under_other_key
+  end
 
+  test "it removes values from distinct sets under distinct keys", %{cache: cache} do
+    cache = initial_writes(cache)
     :ok = Cache.remove(cache, :key, "value2")
+
     values_under_key = Cache.get(cache, :key)
     assert "value1" in values_under_key
     refute "value2" in values_under_key
 
     :ok = Cache.remove(cache, :other_key, "other value")
     refute "other value" in Cache.get(cache, :other_key)
+  end
+
+  ### PRIVATE FUNCTIONS
+
+  defp initial_writes(cache) do
+    :ok = Cache.put(cache, :key, "value1")
+    :ok = Cache.put(cache, :key, "value2")
+    :ok = Cache.put(cache, :other_key, "other value")
+    cache
   end
 end
