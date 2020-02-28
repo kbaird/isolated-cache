7dcfd61a0d4a559fa17a086a0b935f9257b2545e
Kevin Baird <kevincbaird@gmail.com>
2020-02-27 16:10:54 -0800

    Add dialyzer

--- mix.exs
+++ mix.exs
@@ -3,6 +3,18 @@ defmodule Cache.MixProject do
 
   def project do
     [
+      dialyzer: [
+        plt_add_deps: :transitive,
+        plt_add_apps: [:mix, :ex_unit],
+        flags: [
+          :unmatched_returns,
+          :error_handling,
+          :race_conditions,
+          :no_opaque
+        ],
+        ignore_warnings: ".dialyzer_ignore.exs",
+        list_unused_filters: true
+      ],
       app: :cache,
       version: "0.1.0",
       elixir: "~> 1.10",
@@ -21,7 +33,8 @@ defmodule Cache.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.7", only: [:dev, :test], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
