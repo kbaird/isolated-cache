638d005b300bf749b5560b71b2a98c7b5a64e36d
Kevin Baird <kevincbaird@gmail.com>
2020-11-19 17:28:34 -0800

    Separate clauses of start_link/0 and /1

--- lib/cache.ex
+++ lib/cache.ex
@@ -13,6 +13,7 @@ defmodule Cache do
   @typep key :: term()
   @typep get_response :: {:ok, term()} | {:error, :not_found}
   @typep change_response :: :ok | {:error, term()}
+  @typep start_link_response :: {:ok, pid()} | {:error, term()} | :ignore
 
   ### BOILERPLATE
 
@@ -29,8 +30,11 @@ defmodule Cache do
     {:ok, %{kvs: %{}, opts: opts}}
   end
 
-  @spec start_link(Keyword.t()) :: {:ok, pid()} | {:error, term()} | :ignore
-  def start_link(opts \\ []) do
+  @spec start_link() :: start_link_response()
+  def start_link, do: start_link([])
+
+  @spec start_link(Keyword.t()) :: start_link_response()
+  def start_link(opts) do
     server_opts = Keyword.take(opts, [:name])
     GenServer.start_link(__MODULE__, opts, server_opts)
   end
@@ -59,14 +63,12 @@ defmodule Cache do
   end
 
   def handle_cast({:put, key, val}, %{kvs: kvs} = data) do
-    state = state(kvs, key)
-    new_set = MapSet.put(state, val)
+    new_set = kvs |> state(key) |> MapSet.put(val)
     {:noreply, put_in(data, [:kvs, key], new_set)}
   end
 
   def handle_cast({:delete, key, val}, %{kvs: kvs} = data) do
-    state = state(kvs, key)
-    new_set = MapSet.delete(state, val)
+    new_set = kvs |> state(key) |> MapSet.delete(val)
     {:noreply, put_in(data, [:kvs, key], new_set)}
   end
 
