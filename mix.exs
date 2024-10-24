dcbd2bd8d0c0583d3dfa14bbcfed403a617fc5f1
Kevin Baird <kevincbaird@gmail.com>
2024-10-24 07:03:24 -0700

    Bump versions

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule Cache.MixProject do
       ],
       app: :cache,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
